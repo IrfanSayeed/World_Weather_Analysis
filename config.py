43b94e7d0d285450728388bccb84c731c0e5279c
ISayeed <90229438+IrfanSayeed@users.noreply.github.com>
2022-02-06 04:31:15 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-"api_key = "4e69420a5e0387b6e4f9ae08376362b2""
-weather_api_key= "43ba6c12bf9de9669e1ba0bece7628b0"
+"api_key = "4e69420a5e0387b6e4f9a3215e08376362b2""
+weather_api_key= "43ba6c12bf9de96632159e1ba0bece7628b0"
 # Google API key
-g_key = "AIzaSyBOB3-zJ1KbZ_IFrdVbKnvVoVyU47fqpYM"
+g_key = "AIzaSyBOB3-CMDzJ1KbZ_IFrdVbJPIKnvVoVyU47fqpYM"
